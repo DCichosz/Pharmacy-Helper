1335c7ab788beb6d598b77299d7ac576f4a67cdf
Damian Cichosz <damian.cichosz97@gmail.com>
2018-08-13 21:12:06 +0200

    consoleex fix

--- Pharmacy/Helpers/ConsoleEx.cs
+++ Pharmacy/Helpers/ConsoleEx.cs
@@ -2,24 +2,22 @@
 
 namespace Pharmacy
 {
+    /// <summary>
+    /// Static class written to change text color in console
+    /// </summary>
     public static class ConsoleEx
     {
-        public static string Color;
-        public static string Txt;
-
         public static void Write(string txt, ConsoleColor color)
         {
-            Txt = txt;
             Console.ForegroundColor = color;
-            Console.Write(Txt);
+            Console.Write(txt);
             Console.ResetColor();
         }
 
         public static void WriteLine(string txt, ConsoleColor color)
         {
-            Txt = txt;
             Console.ForegroundColor = color;
-            Console.WriteLine(Txt);
+            Console.WriteLine(txt);
             Console.ResetColor();
         }
     }
